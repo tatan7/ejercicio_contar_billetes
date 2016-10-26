c28e581f6cbef9d58059936d21e753ef25faeb38
tatan <jonlomed7@hotmail.com>
2016-10-26 17:52:20 -0500

    segunda parte de la subida

--- js/lib/controllers/ctr_contador.js
+++ js/lib/controllers/ctr_contador.js
@@ -29,3 +29,34 @@ app.controller('ControllerBilletes', ['$scope', function ($scope){
         $scope.total = cantidad * valBillete;
     }
 }]);
+var app = app || {};
+var monedas = [1000,500,200,100,50];
+var valMonedas = 0;
+app.controller('ControllerMonedas', ['$scope', function ($scope){
+    $scope.val_selected = 0;
+
+    $scope.selectMonedas = function(value){
+        if(value === 'mil')
+            valMonedas = monedas[0];
+        else if(value === 'dosmil')
+            valMonedas = monedas[1];
+        else if(value === 'cincomil')
+            valMonedas = monedas[2];
+        else if(value === 'diezmil')
+            valMonedas = monedas[3];
+        else if(value === 'veintemil')
+            valMonedas = monedas[4];
+        else if(value === 'cincuentamil')
+            valMonedas = monedas[5];
+        else
+            return false;
+        $scope.val_selected = valBillete;
+    }
+    $scope.calcular = function(){
+        var cantidad = $scope.cantidad;
+        if(cantidad == '')
+            cantidad = 0;
+        
+        $scope.total = cantidad * valBillete;
+    }
+}]);
